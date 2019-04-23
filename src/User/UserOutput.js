a943336c6b433ed8b540214e31e9e9c87153ad22
gokhanDerala <gderala@gmail.com>
2019-04-23 22:17:23 +0300

    added to state

--- src/User/UserOutput.js
+++ src/User/UserOutput.js
@@ -3,7 +3,7 @@ import React from 'react';
 const userOutput = (props) => {
     return(
         <div>
-            <p>User username is {props.username}</p> 
+            <p>User username is {props.userName}</p> 
             <p>Second Paragrap</p>
         </div>   
     )
