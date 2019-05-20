ff6f30ead3d74b1658d516963345071be21a07d7
gokhanDerala <gderala@gmail.com>
2019-05-20 21:18:22 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,43 +3,6 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
-  state = {
-    persons: [
-      {id:'asfa1',name : "Gokhan", age : 32},
-      {id:'asfa2',name : "Test", age : 40},
-      {id:'asfa3',name : "Test 2", age : 42}
-    ],
-    otherState : 'some other value',
-    showPersons: false
-  }
-
-  nameChangedHandler = (event,id) => {
-
-    const personIndex = this.state.persons.findIndex(p => { return p.id === id });
-
-    const person = {...this.state.persons[personIndex]};
-
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons];
-
-    persons[personIndex] = person;
-
-    this.setState({persons: persons})
-  }
-
-  deletePersonHandler = (personIndex) => {
-    //const persons = this.state.persons.slice();
-    const persons = [...this.state.persons];
-    persons.splice(personIndex,1);
-    this.setState({persons: persons});
-  }
-
-  // Hide div
-  togglerPersonsHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow}); 
-  }
 
   render() {
 
@@ -49,24 +12,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
-    }
-
-    let persons = null;
-
-    if (this.state.showPersons){
-      persons = (
-        <div>
-          {this.state.persons.map((person,index) => {
-            return <Person 
-            click={()=> this.deletePersonHandler(index)} 
-            name={person.name} 
-            age={person.age}
-            key={person.id} 
-            changed = {(event) => this.nameChangedHandler(event, person.id)}
-            />
-          })}
-        </div>
-      );
     }
 
     return (
