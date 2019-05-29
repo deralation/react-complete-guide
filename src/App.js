d07a329a77e0b9a2ec3ea014297b7bfa87466e19
Gokhan Derala <gderala@gmail.com>
2019-05-30 00:58:30 +0300

    hover added

--- src/App.js
+++ src/App.js
@@ -50,7 +50,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -71,6 +75,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[":hover"]={
+        backgroundColor: 'lightred',
+        color: 'black'
+      }
     }
 
     const classes = [];
