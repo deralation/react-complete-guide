f7eeff2b17214fe0ceea024893daa1374afc1480
Gokhan Derala <gderala@gmail.com>
2019-05-29 22:51:23 +0300

    radium package for media queries

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
-        <p className={classes}>This is really working.</p>
+        <p className={classes.join(' ')}>This is really working.</p>
         <button 
         style={style}
         onClick={this.togglerPersonsHandler}>Toggle Person</button>
