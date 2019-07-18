e19a0f69898278ce41f3d3b2f367825f57ed80b8
Gokhan Derala <gderala@gmail.com>
2019-07-18 14:13:01 +0300

    context globally reach the state to avoid the chain props issue

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
+import AuthContext from '../../context/auth-context';
 
 const cockpit = (props) => {
 
@@ -48,7 +49,9 @@ const cockpit = (props) => {
                 className={btnClass}
                 onClick={props.clicked}>Toggle Person
             </button>
-            <button onClick={props.login}>Log in</button>
+            <AuthContext.Consumer>
+                {(context) => <button onClick={context.login}>Log in</button>}
+            </AuthContext.Consumer>
         </div>
     );
 };
